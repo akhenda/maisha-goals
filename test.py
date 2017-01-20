e0fc96e21e4154ce4716a8c671f5047702d32914
andela-akhenda <joseph.akhenda@andela.com>
2017-01-20 19:34:03 +0300

    [Chore] Added coverage to tests

--- test.py
+++ test.py
@@ -1,3 +1,12 @@
 import unittest
+import coverage
 from tests import alltests
+
+
+COV = coverage.coverage(branch=True, include='app/*')
+COV.start()
+
 unittest.TextTestRunner(verbosity=2).run(alltests)
+
+COV.stop()
+COV.report()
