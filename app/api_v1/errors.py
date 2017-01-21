2217fcc9e09e37543e8f24ed82602d9718ca71bc
andela-akhenda <joseph.akhenda@andela.com>
2017-01-21 22:26:10 +0300

    [Chore] Created some custom error handlers

--- app/api_v1/errors.py
+++ app/api_v1/errors.py
@@ -5,19 +5,31 @@ from . import api
 
 @api.errorhandler(ValidationError)
 def bad_request(e):
-    pass
+    res = jsonify({'status': 400, 'error': 'bad request',
+                  'message': e.args[0]})
+    res.status_code = 400
+    return res
 
 
 @api.app_errorhandler(404)
 def not_found(e):
-    pass
+    res = jsonify({'status': 404, 'error': 'not found',
+                  'message': 'invalid resource URL'})
+    res.status_code = 404
+    return res
 
 
 @api.errorhandler(405)
 def method_not_supported(e):
-    pass
+    res = jsonify({'status': 405, 'error': 'method not supported',
+                  'message': 'the method is not supported'})
+    res.status_code = 405
+    return res
 
 
 @api.app_errorhandler(500)
 def internal_server_error(e):
-    pass
+    res = jsonify({'status': 500, 'error': 'internal server error',
+                  'message': e.args[0]})
+    res.status_code = 500
+    return res
