0d37c4f9a3b2acd2edd8f822c0930602687a9158
andela-akhenda <joseph.akhenda@andela.com>
2017-01-23 00:34:22 +0300

    [Chore] Added an about package file

---
+++ app/__about__.py
@@ -0,0 +1,9 @@
+__title__ = 'Maisha Goals'
+__package_name__ = 'app'
+__author__ = 'Joseph AKhenda'
+__description__ = 'This is a Flask API implementing the classic bucketlist ' \
+                'application.'
+__email__ = 'joseph.akhenda@andela.com'
+__version__ = '0.1'
+__license__ = 'MIT'
+__copyright__ = 'Copyright 2017 Joseph AKhenda'
