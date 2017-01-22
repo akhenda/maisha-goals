a599f9775e7935eb88fe6b28fe998c1f233e79f4
andela-akhenda <joseph.akhenda@andela.com>
2017-01-23 00:28:03 +0300

    [Chore] Added the users endpoint test case

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,12 +1,14 @@
 import unittest
 from .test_auth import TestAuth
+from .test_users import TestUsers
 from .test_endpoints import TestEndpoints
 from .test_bucketlists import TestBucketlists
 from .test_bucketlist_items import TestBucketlistItems
 
 suite1 = unittest.TestLoader().loadTestsFromTestCase(TestAuth)
-suite2 = unittest.TestLoader().loadTestsFromTestCase(TestEndpoints)
-suite3 = unittest.TestLoader().loadTestsFromTestCase(TestBucketlists)
-suite4 = unittest.TestLoader().loadTestsFromTestCase(TestBucketlistItems)
+suite2 = unittest.TestLoader().loadTestsFromTestCase(TestUsers)
+suite3 = unittest.TestLoader().loadTestsFromTestCase(TestEndpoints)
+suite4 = unittest.TestLoader().loadTestsFromTestCase(TestBucketlists)
+suite5 = unittest.TestLoader().loadTestsFromTestCase(TestBucketlistItems)
 
-alltests = unittest.TestSuite([suite1, suite2, suite3, suite4])
+alltests = unittest.TestSuite([suite1, suite2, suite3, suite4, suite5])
---
+++ tests/test_users.py
@@ -0,0 +1,61 @@
+from .test_base import TestBase
+
+
+class TestUsers(TestBase):
+    """ Test users' Buckelists """
+
+    def test_get_users(self):
+        res, json = self.client.get('/api/v1/users/')
+        self.assertEqual(res.status_code, 200)
+
+    def test_get_user(self):
+        """ Test that we can fetch a single user """
+        # Get user whose ID is 1
+        res1, json1 = self.client.get('/api/v1/users/1')
+        self.assertEqual(res1.status_code, 200)
+        self.assertEqual('Jack', json1['username'])
+
+        # Get user whose ID is 2
+        res2, json2 = self.client.get('/api/v1/users/2')
+        self.assertEqual(res2.status_code, 200)
+        self.assertEqual('Ronon', json2['username'])
+
+    def test_update_user(self):
+        """ Test editing of user information """
+        res, json = self.client.post('/auth/register',
+                                     data={
+                                        'username': 'Jaffa',
+                                        'password': '123'
+                                     })
+        self.assertEqual(res.status_code, 201)
+        self.assertEqual(
+            json['message'],
+            "Your account has been successfuly created"
+        )
+        location = res.headers['Location']
+        res1, json1 = self.client.put(location, data={"password": "1234"})
+        self.assertEqual(res1.status_code, 200)
+        self.assertTrue(
+            json1['message'],
+            "User successfuly updated"
+        )
+
+    def test_delete_user_account(self):
+        """ Test deletion of a user account """
+        res, json = self.client.post('/auth/register',
+                                     data={
+                                        'username': 'Apophis',
+                                        'password': 'pass'
+                                     })
+        self.assertEqual(res.status_code, 201)
+        self.assertEqual(
+            json['message'],
+            "Your account has been successfuly created"
+        )
+        location = res.headers['Location']
+        res1, json1 = self.client.delete(location)
+        self.assertEqual(res1.status_code, 200)
+        self.assertTrue(
+            json1['message'],
+            "User successfuly deleted"
+        )
