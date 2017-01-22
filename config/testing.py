e9bc67264967afbd3e08fa9ce9e759c8d1032606
andela-akhenda <joseph.akhenda@andela.com>
2017-01-22 16:47:30 +0300

    [Chore] Updated the Server Name

--- config/testing.py
+++ config/testing.py
@@ -6,6 +6,6 @@ db_path = os.path.join(basedir, '../app/databases/test.db')
 DEBUG = False
 TESTING = True
 SECRET_KEY = 'SqCgW6kUtw0ypcjfl379'
-SERVER_NAME = 'e.com'
+SERVER_NAME = 'maisha-goals.com'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + db_path
 SQLALCHEMY_TRACK_MODIFICATIONS = False
