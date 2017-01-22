b39bfd1138deadf7af15c01eff439e346e9cf50b
andela-akhenda <joseph.akhenda@andela.com>
2017-01-22 20:46:35 +0300

    [Feature] Added validations for query parameters

--- app/decorators/paginate.py
+++ app/decorators/paginate.py
@@ -1,5 +1,6 @@
 import functools
 from flask import url_for, request
+from ..exceptions import ValidationError
 
 
 def paginate(collection, max_per_page=20):
@@ -20,37 +21,44 @@ def paginate(collection, max_per_page=20):
 
             # obtain pagination arguments from the URL's query string
             page = request.args.get('page', 1, type=int)
-            per_page = min(request.args.get('per_page', max_per_page,
-                                            type=int), max_per_page)
-            expanded = None
-            if request.args.get('expanded', 0, type=int) == 0:
+            if request.args and (not request.args.get('limit') and not request.args.get('q') and not request.args.get('expanded') and not request.args.get('page')):
+                raise ValidationError('invalid query parameter')
+            elif request.args.get('limit'):
+                try:
+                    limit = int(request.args.get('limit'))
+                except:
+                    raise ValidationError('limit query parameter only accepts integers')
+            limit = min(request.args.get('limit', max_per_page,
+                                         type=int), max_per_page)
+            expanded = 1
+            if request.args.get('expanded', 0, type=int) != 0:
                 expanded = 1
 
             # run the query with Flask-SQLAlchemy's pagination
-            p = query.paginate(page, per_page)
+            p = query.paginate(page, limit)
 
             # build the pagination metadata to include in the response
-            pages = {'page': page, 'per_page': per_page,
+            pages = {'page': page, 'limit': limit,
                      'total': p.total, 'pages': p.pages}
             if p.has_prev:
                 pages['prev_url'] = url_for(request.endpoint, page=p.prev_num,
-                                            per_page=per_page,
+                                            limit=limit,
                                             expanded=expanded, _external=True,
                                             **kwargs)
             else:
                 pages['prev_url'] = None
             if p.has_next:
                 pages['next_url'] = url_for(request.endpoint, page=p.next_num,
-                                            per_page=per_page,
+                                            limit=limit,
                                             expanded=expanded, _external=True,
                                             **kwargs)
             else:
                 pages['next_url'] = None
             pages['first_url'] = url_for(request.endpoint, page=1,
-                                         per_page=per_page, expanded=expanded,
+                                         limit=limit, expanded=expanded,
                                          _external=True, **kwargs)
             pages['last_url'] = url_for(request.endpoint, page=p.pages,
-                                        per_page=per_page, expanded=expanded,
+                                        limit=limit, expanded=expanded,
                                         _external=True, **kwargs)
 
             # generate the paginated collection as a dictionary
