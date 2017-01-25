a939f43eff5483facc2ab2d55cc6b3b0f9d4d9c0
andela-akhenda <joseph.akhenda@andela.com>
2017-01-25 23:10:40 +0300

    [Fix] Changed the configuration files to import the DB file correctly

--- config/development.py
+++ config/development.py
@@ -1,8 +1,7 @@
 import os
 
-basedir = os.path.abspath('.')
-db_path = os.path.join(basedir, 'app/databases/dev.db').strip('/')
-
+basedir = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(basedir, '../app/databases/dev.db')
 
 DEBUG = True
 IGNORE_AUTH = True
--- config/testing.py
+++ config/testing.py
@@ -1,13 +1,11 @@
 import os
 
-basedir = os.path.abspath('.')
-os.chdir(basedir)
-# db_path = (basedir + '/app/databases/test.db').strip('/')
-
+basedir = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(basedir, 'test.db')
 
 DEBUG = False
 TESTING = True
 SECRET_KEY = 'SqCgW6kUtw0ypcjfl379'
 SERVER_NAME = 'maisha-goals.com'
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.getcwd() + '/app/databases/test.db'
+SQLALCHEMY_DATABASE_URI = 'sqlite:///' + db_path
 SQLALCHEMY_TRACK_MODIFICATIONS = False
