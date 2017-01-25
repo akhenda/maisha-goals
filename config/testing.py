b1beda806fe2da7c0dd1b78045150323a178f180
andela-akhenda <joseph.akhenda@andela.com>
2017-01-25 14:34:31 +0300

    [Fix] Reverted back to initial configs

--- config/testing.py
+++ config/testing.py
@@ -1,10 +1,11 @@
 import os
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-db_path = os.path.join(basedir, 'test.db')
+db_path = os.path.join(basedir, '../app/databases/test.db')
 
 DEBUG = False
 TESTING = True
 SECRET_KEY = 'SqCgW6kUtw0ypcjfl379'
+SERVER_NAME = 'maisha-goals.com'
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + db_path
 SQLALCHEMY_TRACK_MODIFICATIONS = False
